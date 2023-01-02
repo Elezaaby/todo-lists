3de8f54a7b693bbef4a47baa32535e8acb39ceba
Elezaaby <mahmodalezaby@gmail.com>
2023-01-02 14:22:37 +0200

    Try to fix the second error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import './App.css';
-import { useEffect, useState } from 'react';
-import jwtDecode from 'jwt-decode';
-import { useNavigate } from 'react-router-dom';
 import { UserDataContextProvider } from './UserDataContext';
 
 
